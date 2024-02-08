7c3f3e47b1683d7dd5e896a4efa9c374cb222bdb
sakai-ysk <ysphaplife@gmail.com>
2024-02-08 11:34:50 +0900

    step8_2

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product; // Productモデルを使用
 use App\Models\Sale; // Saleモデルを使用
+use Illuminate\Support\Facades\DB;
 
 class SalesController extends Controller
 {
     //
     public function purchase(Request $request)
 {
+    try {
+            // トランザクションの開始
+            DB::beginTransaction();
+
     // リクエストから必要なデータを取得する
     $productId = $request->input('product_id'); // "product_id":7が送られた場合は7が代入される
     $quantity = $request->input('quantity', 1); // 購入する数を代入する もしも”quantity”というデータが送られていない場合は1を代入する
@@ -39,6 +44,12 @@ class SalesController extends Controller
 
 
     $sale->save();
+
+    DB::commit();
+    } catch (\Exception $e) {
+        DB::rollback();
+        return back();
+    }
 
     // レスポンスを返す
     return response()->json(['message' => '購入成功']);
